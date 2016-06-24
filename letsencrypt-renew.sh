4f6390890a674aed4a86f4eb7a427922276eec93
David Lukes <dafydd.lukes@gmail.com>
2016-06-24 10:03:26 +0200

    remove output on letsencrypt cronjob success

--- letsencrypt-renew.sh
+++ letsencrypt-renew.sh
@@ -6,8 +6,6 @@ LE_STATUS=$?
 systemctl start nginx
 if [ "$LE_STATUS" != 0 ]; then
     echo Automated letsencrypt renewal failed:
-    cat /var/log//letsencrypt/renew.log
+    cat /var/log/letsencrypt/renew.log
     exit 1
-else
-    echo "$0: No errors."
 fi
